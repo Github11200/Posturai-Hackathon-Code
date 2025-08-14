bcef01a740e19c085c14f271f413026f2547b220
Github11200 <pateljinay@outlook.com>
2025-08-13 18:04:09 -0700

    fixed the build errors

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -8,6 +8,19 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: { esmExternals: true },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
+    return config;
+  },
+  experimental: {
+    esmExternals: true,
+  },
+  transpilePackages: ["onnxruntime-web"],
 };
 
 export default nextConfig;
